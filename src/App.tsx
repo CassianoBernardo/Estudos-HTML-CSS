96bb1401245f8e95abf81b0b2cd5412d8a87a631
CassianoBernardo <cassianokina@gmail.com>
2026-06-19 10:44:54 -0300

    feat: estudos HTML e CSS com listas, cores e espaçamento

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <nav>
+      <ul>
+          <li>Início</li>
+          <li>Mapa</li>
+          <li>Sobre</li>
+          <li>Contato</li>
+        </ul>
+      </nav>
+      <p>Este é um parágrafo</p>
+      <p className='maior'>Este é outro parágrafo</p>
+      <ul>
+        <li>Início</li>
+        <li>Mapa</li>
+        <li>Sobre</li>
+        <li>Contato</li>
+      </ul>
+      <button>Botão</button>
+      
+  
+
+    </>
+  )
+}
+
+export default App
